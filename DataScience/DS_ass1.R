3e23fd03ba00a5cec83b2679b8b82564c332bb85
Puranjay Singh <singh.puranjay14@gmail.com>
2022-10-20 22:07:41 +0530

    Final DS

---
+++ DataScience/DS_ass1.R
@@ -0,0 +1,45 @@
+A <- 23.4
+B <- 45
+C <- 678
+print(A)
+print(B)
+print(C)
+
+my_list <- list(A,B)
+my_list
+
+#I am learning R
+
+firstname = "MyName"
+lastname = "MySurname"
+print(firstname)
+print(lastname)
+
+boolvar = T
+print(boolvar)
+
+ans = A-B+C
+print(ans)
+ans = A*B-C
+print(ans)
+ans = C/B+A
+print(ans)
+ans = A+B-C
+print(ans)
+
+print(exp(2))
+print(log(10))
+print(log10(10))
+print(log2(10))
+print(pi)
+print(sqrt(64))
+
+ans = 23 + (4.5*2.3)/10
+print(ans)
+ans = 456/12 - log(90)
+print(ans)
+ans = exp(5) + 12/(5^6)
+print(ans)
+ans = sqrt(45)*12/3
+print(ans)
+
